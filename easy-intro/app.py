06ebe0a4294d0b6e0a864da475ee59e42f5a007a
Ashwath Nandakumar <Ashwath.Nandakumar@integrate.com>
2024-07-03 16:16:27 +0530

    minor refactoring

--- easy-intro/app.py
+++ easy-intro/app.py
@@ -1,10 +1,8 @@
-import logging
 from pathlib import Path
 
 from dotenv import load_dotenv
 from litestar import Litestar, get
 from litestar.exceptions import InternalServerException
-from litestar.logging import LoggingConfig
 from litestar.static_files import create_static_files_router
 from openai import RateLimitError
 
@@ -14,6 +12,8 @@ from exception_handler import app_exception_handler, router_handler_exception_ha
 load_dotenv()
 
 HTML_DIR = Path("templates")
+
+
 def on_startup() -> None:
     HTML_DIR.mkdir(exist_ok=True)
     HTML_DIR.joinpath("404.html").write_text("<h1>Not found</h1>")
@@ -21,7 +21,6 @@ def on_startup() -> None:
 
 @get("/profile-summary", sync_to_thread=False, exception_handlers={RateLimitError: router_handler_exception_handler})
 def profile_summary(user_name: str) -> dict[str, str]:
-
     try:
         summary, profile_url = ice_break_with(user_name)
         return {
@@ -37,7 +36,7 @@ app = Litestar(route_handlers=[
     create_static_files_router(
             path="/",
             directories=["templates"],
-            html_mode=True,)
+            html_mode=True)
     ],
     exception_handlers={InternalServerException: app_exception_handler},
     debug=True)
